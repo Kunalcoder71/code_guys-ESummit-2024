7f30930d3b33fc694e8a7587033dfef317163965
Kunal <kunalrajak979@gmail.com>
2024-11-17 00:42:40 +0530

    ui design

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CreateRoom from './components/CreateRoom';
+import JoinRoom from './components/JoinRoom';
+import Room from './components/Room';
+import VideoRoom from './components/VideoRoom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <div className='firstdiv'>
+          <h1 className='welcome-text'>Welcome to Live Video Room</h1>
+          <JoinRoom />
+          <CreateRoom />
+        </div>} />
+        <Route path="/room/:roomKey" element={<VideoRoom />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Suppress ResizeObserver loop limit errors
+const resizeObserverErr = window.ResizeObserver.prototype.observe;
+window.ResizeObserver.prototype.observe = function() {
+  try {
+    resizeObserverErr.apply(this, arguments);
+  } catch (e) {
+    console.warn('ResizeObserver loop error suppressed');
+  }
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// Suppress ResizeObserver loop limit errors
